b88f09bbf1e7cc78a3b8671f7dabdffe633a8c66
Timo Mätäsaho <timo.matasaho@gmail.com>
2015-09-16 15:28:14 +0300

    Initial commit

---
+++ stockparser.py
@@ -0,0 +1,12 @@
+from HTMLParser import HTMLParser
+
+class stockParser(HTMLParser):
+	def handle_starttag(self, tag, attrs):
+
+	def handle_endtag(self, tag):
+
+	def handle_data(self, data):
+
+	def handle_comment(self, data):
+
+
---
+++ stocktracker.py
@@ -0,0 +1,12 @@
+import urllib2
+from stockparser import stockParser
+
+testurl = "http://www.kauppalehti.fi/5/i/porssi/porssikurssit/osake/?klid=1059"
+url = testurl
+
+def main():
+	response = urllib.urlopen( url )
+	feed = response.read()
+	parser = stockParser()
+
+main()
