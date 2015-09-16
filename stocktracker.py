67d50bb24bfbc4fa101c89178c0df5978c104d32
unknown <oultmata@OULL075.ad.aavamobile.com>
2015-09-16 15:30:17 +0300

    testing

--- stocktracker.py
+++ stocktracker.py
@@ -2,7 +2,7 @@ import urllib2
 from stockparser import stockParser
 
 testurl = "http://www.kauppalehti.fi/5/i/porssi/porssikurssit/osake/?klid=1059"
-url = testurl
+url = testur
 
 def main():
 	response = urllib.urlopen( url )
