921d055e805dc98228d54390f5959ca117805e88
Timo Mätäsaho <timo.matasaho@gmail.com>
2015-09-18 16:34:52 +0300

    Added BeautifulSoup and started playing with it. Probably more convenient solution than building own parser.

--- stockparser.py
+++ stockparser.py
@@ -1,12 +1,62 @@
 from HTMLParser import HTMLParser
 
 class stockParser(HTMLParser):
-	def handle_starttag(self, tag, attrs):
+	# Overridable -- handle start tag
 
-	def handle_endtag(self, tag):
+    def handle_starttag(self, tag, attrs):
 
-	def handle_data(self, data):
-
-	def handle_comment(self, data):
+    	pass
 
 
+    # Overridable -- handle end tag
+
+    def handle_endtag(self, tag):
+
+        pass
+
+
+    # Overridable -- handle character reference
+
+    def handle_charref(self, name):
+
+        pass
+
+
+    # Overridable -- handle entity reference
+
+    def handle_entityref(self, name):
+
+        pass
+
+
+    # Overridable -- handle data
+
+    def handle_data(self, data):
+
+        pass
+
+
+    # Overridable -- handle comment
+
+    def handle_comment(self, data):
+
+        pass
+
+
+    # Overridable -- handle declaration
+
+    def handle_decl(self, decl):
+
+        pass
+
+
+    # Overridable -- handle processing instruction
+
+    def handle_pi(self, data):
+
+        pass
+
+
+    def unknown_decl(self, data):
+
+        pass
--- stocktracker.py
+++ stocktracker.py
@@ -1,11 +1,28 @@
 import urllib2
+import os
+from bs4 import BeautifulSoup
 from stockparser import stockParser
 
-testurl = "http://www.kauppalehti.fi/5/i/porssi/porssikurssit/osake/?klid=1059"
+url = "http://www.kauppalehti.fi/5/i/porssi/porssikurssit/osake/?klid=1059"
+#sock = open('testhtml.html', 'r')
+#feed = sock.read()
+#sock.close()
 
 def main():
-	response = urllib.urlopen( url )
-	feed = response.read()
-	parser = stockParser()
+
+	clear = lambda: os.system('cls')
+	clear()
+
+	sock = urllib2.urlopen( url )
+	feed = sock.read()
+
+	#soup = BeautifulSoup(open("testhtml.html"), 'lxml')
+	soup = BeautifulSoup( feed, 'lxml' )
+
+	print soup.table
+
+	#interestingBlock = soup.find_all('table', class_="table_stockexchange")
+
+	#print interestingBlock
 
 main()
