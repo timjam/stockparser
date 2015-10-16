31754a52f6ee59f81868029384038bb4ac0caf60
Timo Mätäsaho <timo.matasaho@gmail.com>
2015-10-16 15:33:45 +0300

    Cleaned the code. Now should find the correct div element

--- stocktracker.py
+++ stocktracker.py
@@ -3,21 +3,15 @@
 
 import urllib2
 import os
-from bs4 import BeautifulSoup
 from stockparser import stockParser
 from lxml import etree
 
 url = "http://www.kauppalehti.fi/5/i/porssi/porssikurssit/osake/?klid=1059" #Wärtsilä
 url2 = "http://www.kauppalehti.fi/5/i/porssi/porssikurssit/osake/?klid=1091" #Sanoma
-#sock = open('testhtml.html', 'r')
-#feed = sock.read()
-#sock.close()
 
 def main():
 
 	os.system('cls')
-	#clear = lambda: os.system('cls')
-	#clear()
 
 	sock = urllib2.urlopen( url )
 	feed = sock.read()
@@ -33,45 +27,11 @@ def main():
 	print title
 
 
-	# Extract the interesting div "tunnuslukuja"
+	# Extract the interesting div which contains the <p> "tunnuslukuja"
 
-	#rootDiv = data.xpath("//div[contains(@class='white_box')]/p[@text='Tunnuslukuja']")
-	rootDiv = data.xpath(".//div[@class='box_white' and ./p[contains(., 'Tunnuslukuja')]]")
+	rootDiv = data.xpath(".//p[text()='Tunnuslukuja']")[0].getparent() # selects a <p> with text 'Tunnuslukuja', creates a list of it, chooses the first element from the list (it is known that there's only one element) and gets its parent
 
-	for element in rootDiv:
-		print element.tag, element.attrib
+	print rootDiv.tag, rootDiv.attrib
 
-	#print rootDiv.tag, rootDiv.attrib
-
-	#rootDiv2 = data.xpath("/p[text()='Tunnuslukuja']/..")
-	rootDiv2 = data.xpath(".//p[text()='Tunnuslukuja']")
-
-
-	for el in rootDiv2:
-		print el.tag, el.attrib, el.text, el.getparent().tag, el.getparent().attrib
-
-
-	# BFSoup things here
-
-	#soup = BeautifulSoup(open("testhtml.html"), 'lxml')
-	soup = BeautifulSoup( feed, 'lxml' )
-
-	title = soup.find("title")
-	stockname = soup.find("div", {"id": "EAS_11667"})
-	interestingDiv = soup.find_all("p", text="Tunnuslukuja")
-
-	#print interestingDiv
-	#print stockname
-	#print title
-
-	#for elems in interestingDiv:
-	#	for row in elems.find_all("p", text="Tunnuslukuja"):
-	#		print row
-
-
-
-	#interestingBlock = soup.find_all('table', class_="table_stockexchange")
-
-	#print interestingBlock
 
 main()
